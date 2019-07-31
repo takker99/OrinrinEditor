3aa200494fe57fb0ce46bed2624832e3e48a5789
Duct-and-rice <nikuinemitsubishi@gmail.com>
2019-08-01 00:12:55 +0900

    fix zip

--- package.ps1
+++ package.ps1
@@ -3,7 +3,7 @@
 
     Copy-Item -Path .\x64\Release\OrinrinEditor.exe -Destination .\OrinrinEditor-x64\
     Copy-Item -Path .\x64\Release\sqlite3.dll -Destination .\OrinrinEditor-x64\
-    Compress-Archive -DestinationPath OrinrinEditor-x64.zip -Path .\OrinrinEditor-x64
+    Start-Process -FilePath "C:\Program Files\7-Zip\7z.exe" -ArgumentList "a OrinrinEditor-x64.zip OrinrinEditor-x64\"
 }
 
 if(Test-Path .\Release\){
@@ -12,5 +12,5 @@ if(Test-Path .\Release\){
     Copy-Item -Path .\Release\OrinrinEditor.exe -Destination .\OrinrinEditor-x86\
     Copy-Item -Path .\Release\sqlite3.dll -Destination .\OrinrinEditor-x86\
     Copy-Item -Path .\Library\lib\imgctl.dll -Destination .\OrinrinEditor-x86\
-    Compress-Archive -DestinationPath OrinrinEditor-x86.zip -Path .\OrinrinEditor-x86
+    Start-Process -FilePath "C:\Program Files\7-Zip\7z.exe" -ArgumentList "a OrinrinEditor-x86.zip OrinrinEditor-x86\"
 }
