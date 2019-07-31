0e768390a8f4438a1a987158d145aae01c2f57c2
Duct-and-rice <nikuinemitsubishi@gmail.com>
2019-07-31 17:23:47 +0900

    x86 build

--- package.ps1
+++ package.ps1
@@ -1,3 +1,11 @@
-﻿Copy-Item -Path ./x64/Release/OrinrinEditor.exe -Destination ./package
-Copy-Item -Path ./x64/Release/sqlite3.dll -Destination ./package
-Compress-Archive -DestinationPath OrinrinEditor.zip -Path ./package/*
+﻿Copy-Item -Path .\package -Destination .\OrinrinEditor-x64\ -Recurse
+Copy-Item -Path .\package\ -Destination .\OrinrinEditor-x86\ -Recurse
+
+Copy-Item -Path .\x64\Release\OrinrinEditor.exe -Destination .\OrinrinEditor-x64\
+Copy-Item -Path .\x64\Release\sqlite3.dll -Destination .\OrinrinEditor-x64\
+Compress-Archive -DestinationPath OrinrinEditor-x64.zip -Path .\OrinrinEditor-x64
+
+Copy-Item -Path .\Release\OrinrinEditor.exe -Destination .\OrinrinEditor-x86\
+Copy-Item -Path .\Release\sqlite3.dll -Destination .\OrinrinEditor-x86\
+Copy-Item -Path .\Library\lib\imgctl.dll -Destination .\OrinrinEditor-x86\
+Compress-Archive -DestinationPath OrinrinEditor-x86.zip -Path .\OrinrinEditor-x86
